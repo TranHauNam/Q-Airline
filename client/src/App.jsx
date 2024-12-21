da1af0f3d36f59f43dc5e013022155add76b8af2
DangThanhWang <125245112+DangThanhWang@users.noreply.github.com>
2024-12-21 10:16:22 +0700

    my-bookings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import FlightSearchPage from './pages/FlightSearchPage';
 import BookingPage from './pages/BookingPage';
-import BookingManagementPage from './pages/BookingManagementPage';
 import AdminDashboard from './pages/admin/AdminDashboard';
 import FlightManagement from './pages/admin/FlightManagement';
 import NewsManagement from './pages/admin/NewsManagement';
 import BookingConfirmationPage from './pages/BookingConfirmationPage';
+import UserBookingsPage from './pages/UserBookingsPage';
 
 const App = () => {
   return (
@@ -15,8 +15,8 @@ const App = () => {
         <Route path="/" element={<FlightSearchPage />} />
         <Route path="/flights" element={<FlightSearchPage />} />
         <Route path="/booking" element={<BookingPage />} />
-        <Route path="/manage-booking" element={<BookingManagementPage />} />
         <Route path="/booking-confirmation" element={<BookingConfirmationPage />} />
+        <Route path="/my-bookings" element={<UserBookingsPage />} />
         
         {/* Routes cho admin */}
         <Route path="/admin" element={<AdminDashboard />} />
